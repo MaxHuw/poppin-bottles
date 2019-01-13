c49f712bee10219d36c93d6883dc70e1c1a8677f
Max Graham <m.huw.graham@gmail.com>
2019-01-12 22:08:09 -0500

    Functions with proper output, showing number of left over empties, caps, and how many were earned through trade ins.

--- poppin-bottles.js
+++ poppin-bottles.js
@@ -6,29 +6,42 @@ var popBottles = {
   fullBottles: 0,
   totalReturn: 0,
   emptyBottles: 0,
+  bottleByRecyclingBottle: 0,
   caps: 0,
+  bottleByRecyclingCaps: 0,
 
   calcInvestment: function(fullBottles) {
 
     if (fullBottles <= 0){
-      return;
+      return console.log(
+        `
+      Total Bottles: ${this.totalReturn}
+      Remaining Empty Bottles: ${this.emptyBottles}
+      Remaining Caps: ${this.caps}
+      Total Earned Through Recycling:
+        From Bottles: ${this.bottleByRecyclingBottle}
+        From Caps: ${this.bottleByRecyclingCaps}
+        `);
     }
 
     this.emptyBottles += fullBottles;
     this.caps += fullBottles;
     this.fullBottles = 0;
 
-   // console.log(this.fullBottles, this.emptyBottles, this.caps, this.totalReturn);
+   //console.log(this.fullBottles, this.emptyBottles, this.caps, this.totalReturn, this.bottleByRecyclingBottle, this.bottleByRecyclingCaps);
 
     if (this.emptyBottles >= 2) {
       this.fullBottles += Math.floor(this.emptyBottles / 2);
       this.totalReturn += Math.floor(this.emptyBottles / 2);
+      this.bottleByRecyclingBottle += Math.floor(this.emptyBottles / 2);
       this.emptyBottles -= Math.floor(this.emptyBottles / 2) * 2;
+
     }
 
     if (this.caps >= 4) {
       this.fullBottles += Math.floor(this.caps / 4);
       this.totalReturn += Math.floor(this.caps /4);
+      this.bottleByRecyclingCaps += Math.floor(this.caps / 4);
       this.caps -= Math.floor(this.caps /4) * 4;
     }
 
@@ -48,4 +61,4 @@ popBottles.totalReturn = Math.floor(popBottles.investment / 2);
 
 popBottles.calcInvestment(popBottles.fullBottles);
 
-console.log(popBottles.fullBottles, popBottles.emptyBottles, popBottles.caps, popBottles.totalReturn);
+//console.log(popBottles.fullBottles, popBottles.emptyBottles, popBottles.caps, popBottles.totalReturn);
