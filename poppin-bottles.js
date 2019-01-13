17c7a158a9abd66e584dd2334df0e70c87b895c5
Max Graham <m.huw.graham@gmail.com>
2019-01-13 02:45:15 +0000

    Works, gives the right return values, just need to fix the final printout.

--- poppin-bottles.js
+++ poppin-bottles.js
@@ -1,8 +1,9 @@
 
+
 var popBottles = {
 
-  investment: 10,
-  fullBottles: 5,
+  investment: 0,
+  fullBottles: 0,
   totalReturn: 0,
   emptyBottles: 0,
   caps: 0,
@@ -17,16 +18,18 @@ var popBottles = {
     this.caps += fullBottles;
     this.fullBottles = 0;
 
-    if (this.emptyBottles > 2) {
+   // console.log(this.fullBottles, this.emptyBottles, this.caps, this.totalReturn);
+
+    if (this.emptyBottles >= 2) {
       this.fullBottles += Math.floor(this.emptyBottles / 2);
+      this.totalReturn += Math.floor(this.emptyBottles / 2);
       this.emptyBottles -= Math.floor(this.emptyBottles / 2) * 2;
-      this.returnOnInvestment += Math.floor(this.emptyBottles / 2);
     }
 
-    if (this.caps > 4) {
+    if (this.caps >= 4) {
       this.fullBottles += Math.floor(this.caps / 4);
-      this.caps -= Math.floor(this.caps /4);
-      this.returnOnInvestment += Math.floor(this.caps /4);
+      this.totalReturn += Math.floor(this.caps /4);
+      this.caps -= Math.floor(this.caps /4) * 4;
     }
 
 
@@ -36,7 +39,13 @@ var popBottles = {
 
 };
 
+var startingCap = process.argv.slice(2);
+
+popBottles.investment = Number(startingCap[0]);
+popBottles.fullBottles = Math.floor(popBottles.investment / 2);
+popBottles.totalReturn = Math.floor(popBottles.investment / 2);
+
 
 popBottles.calcInvestment(popBottles.fullBottles);
 
-console.log(popBottles.fullBottles, popBottles.emptyBottles, popBottles.caps);
+console.log(popBottles.fullBottles, popBottles.emptyBottles, popBottles.caps, popBottles.totalReturn);
